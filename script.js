6da8d77eea3e1320e17061ab6d68b26c1c2596d3
AbdulMajidsaddique <majidsaddique2222@gmail.com>
2024-10-30 21:44:26 +0500

    Dynamic gooey effect completed

---
+++ script.js
@@ -0,0 +1,9 @@
+Shery.imageEffect(".images", {
+    style: 3,
+    /*optional parameters
+    these parameter dose not applies to custom scroll trigger callback */
+    scrollSnapping: true,
+    scrollSpeed: 6,
+    touchSpeed: 1,
+    damping: 7,
+  });
